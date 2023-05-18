d5a10aa023656ed8f4cf59739b1c281c81a999ca
Hayden Randall <haydenjrandall@gmail.com>
2023-05-18 09:21:48 -0600

    moved the mongo db configuration into a class and made the api for getting all the ragers work

---
+++ MongoDB.py
@@ -0,0 +1,35 @@
+# This class is for setting up the connection to mongodb
+#pymongo is the library we use to connect to our mongodb
+import pymongo
+from pymongo import MongoClient
+# bson.json_util is the library we use to make the cursor we recieve back from mongodb into a json object
+from bson.json_util import dumps
+class MongoDB:
+    def __init__(self,dbUrl,db):
+        # The dbUrl is the connection string you get from mongo in order to connect to your mongo instance
+        # The db is the name of the database in your mongo instance you want to connect to
+        # The cluster is which "table" you want to access in the database
+        self.mongoUrl = MongoClient(dbUrl,)
+        self.db = self.mongoUrl[db]
+        self.cluster = ""
+
+    # This is to set which "table" you will want to search or make changes to in the CRUD methods
+    def set_cluster(self,cluster):
+        self.cluster = self.db[cluster]
+        return
+    
+    # The next four methods are the CRUD methods for the mongo db
+    def post(self,json):
+        self.cluster.insert_one(json)
+        return
+    def get(self, json):
+        cursor = self.cluster.find(json)
+        list_cursor = list(cursor)
+        json = dumps(list_cursor)
+        return json
+    def delete(self,id):
+        self.cluster.delete_one()
+        return
+    def update(self,id):
+        self.cluster.update({ "id":id},{"$inc": {"reports": 1}})
+        return
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-import pymongo
-from pymongo import MongoClient
-cluster = MongoClient("mongodb+srv://Developer:QjeAr4lwRkKYsiBW@ragerslist.kcn47su.mongodb.net/?retryWrites=true&w=majority")
-db = cluster["RagersUsernames"]
-collection = db["Ragers"]
+from MongoDB import MongoDB
+
+#creat the fastapi app
 app = FastAPI()
-@app.get("/")
-def root():
-    # post = {"hello": 0}
-    # collection.insert_one(post)
-    return {"helo": "Chrisssss"}
+#create an instance of the mongodb that we are connecting to
+db = MongoDB("mongodb+srv://Developer:QjeAr4lwRkKYsiBW@ragerslist.kcn47su.mongodb.net/?retryWrites=true&w=majority","RagersUsernames")
+
+@app.get("/getRagersList")
+def getRagersList():
+    db.set_cluster("Ragers")
+    ragers = db.get({})
+    return ragers 
