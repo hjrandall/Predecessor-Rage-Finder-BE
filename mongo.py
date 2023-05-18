df1c7397a60f48d5f25083e43e377474d9c67222
Hayden Randall <haydenjrandall@gmail.com>
2023-05-17 22:52:14 -0600

    delete test file

--- mongo.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-cluster = MongoClient("mongodb+srv://Developer:QjeAr4lwRkKYsiBW@ragerslist.kcn47su.mongodb.net/?retryWrites=true&w=majority")
-db = cluster["RagersUsernames"]
-collection = db["Ragers"]
-post = {"hello": 0}
-collection.insert_one(post)
